ca837d94ee5a58ea5ed2a166b1c1cfbe917480bb
Jungs <jungs6734@gmail.com>
2020-02-20 18:08:50 +0900

    Chapter 3 - 무브 컴포넌트 활용

--- ArenaBattle/Source/ArenaBattle/Fountain.cpp
+++ ArenaBattle/Source/ArenaBattle/Fountain.cpp
@@ -7,12 +7,17 @@
 AFountain::AFountain()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	// PrimaryActorTick.bCanEverTick = true;
+	
+	// 무브먼트 컴포넌트로 변경해서 이동시킴 불필요하기 때문에 비활성화
+	PrimaryActorTick.bCanEverTick = false;
 
 	Body = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("BODY"));
 	Water = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("WATER"));
 	Light = CreateDefaultSubobject<UPointLightComponent>(TEXT("LIGHT"));
 	Splash = CreateDefaultSubobject<UParticleSystemComponent>(TEXT("SPLASH"));
+
+	Movement = CreateDefaultSubobject<URotatingMovementComponent>(TEXT("MOVEMENT"));
 
 	// 부모를 몸체로 설정
 	RootComponent = Body;
@@ -32,6 +37,10 @@ AFountain::AFountain()
 
 	if (SM_BODY.Succeeded())
 		Body->SetStaticMesh(SM_BODY.Object);
+
+	RotateSpeed = 30.0f;
+
+	Movement->RotationRate = FRotator(0.0f, RotateSpeed, 0.0f);
 }
 
 // Called when the game starts or when spawned
@@ -52,5 +61,18 @@ void AFountain::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+	// 회전 시키기
+	AddActorLocalRotation(FRotator(0.0f, RotateSpeed * DeltaTime, 0.0f));
 }
 
+void AFountain::EndPlay(const EEndPlayReason::Type EndPlayReason)
+{
+	Super::EndPlay(EndPlayReason);
+	ABLOG_S(Warning);
+}
+
+void AFountain::PostInitializeComponents()
+{
+	Super::PostInitializeComponents();
+	ABLOG_S(Warning);
+}
--- ArenaBattle/Source/ArenaBattle/Fountain.h
+++ ArenaBattle/Source/ArenaBattle/Fountain.h
@@ -7,6 +7,9 @@
 
 #include "ArenaBattle.h"
 
+// 언리얼 엔진에서는 움직이라는 요소를 분리해 액터와 별도로 관리하도록 프레임워크를 구성
+#include "GameFramework/RotatingMovementComponent.h"
+
 #include "GameFramework/Actor.h"
 #include "Fountain.generated.h"
 
@@ -20,11 +23,18 @@ public:
 	AFountain();
 
 protected:
-	// Called when the game starts or when spawned
+	// Called when the game starts or when spawned, 액터가 게임에 참여할 때 호출
 	virtual void BeginPlay() override;
 
+	// 게임에서 더 이상 액터의 역할이 없어지면, 액터는 게임에서 퇴장하고 메모리 소멸
+	// 액터가 게임에서 퇴장할 때 호출
+	virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
+
+	// 액터에 속한 모드 컴포넌트의 세팅이 완료되면 호출
+	virtual void PostInitializeComponents() override;
+
 public:	
-	// Called every frame
+	// Called every frame, 매 프레임마다 호출
 	virtual void Tick(float DeltaTime) override;
 
 	// VisibleAnywhere : 컴포넌트 속성을 편집하기 위해 필요한 키워드
@@ -40,6 +50,14 @@ public:
 	UPROPERTY(VisibleAnywhere)
 	UParticleSystemComponent *Splash;
 
+	UPROPERTY(VisibleAnywhere)
+	URotatingMovementComponent* Movement;
+
 	UPROPERTY(EditAnywhere, Category=ID)
 	int32 ID;
+
+private:
+	// 프로그래밍 영역에서 캡슐화 (private : 정보은닉)
+	UPROPERTY(EditAnywhere, Category = Stat, Meta = (AllowPrivateAccess = true))
+	float RotateSpeed;
 };
