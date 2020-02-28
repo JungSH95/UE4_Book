01aae21801ad21e2a5f3b6133df5597f366423fb
Jungs <jungs6734@gmail.com>
2020-02-28 17:31:13 +0900

    Chapter 12 - 라인 에러 해결 &  플레이어 못찾음

--- ArenaBattle/Source/ArenaBattle/BTService_Detect.cpp
+++ ArenaBattle/Source/ArenaBattle/BTService_Detect.cpp
@@ -19,17 +19,16 @@ void UBTService_Detect::TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeM
 {
 	Super::TickNode(OwnerComp, NodeMemory, DeltaSeconds);
 
+	OwnerComp.GetBlackboardComponent()->SetValueAsObject(AABAIController::TargetKey, nullptr);
+
 	APawn* ControllingPawn = OwnerComp.GetAIOwner()->GetPawn();
-	if (nullptr == ControllingPawn)
-		return;
+	if (nullptr == ControllingPawn) return;
 
 	UWorld* World = ControllingPawn->GetWorld();
 	FVector Center = ControllingPawn->GetActorLocation();
-	float DetectRadius = 600.0f;
+	float DetectRadius = 6000.0f;
 
-	if (nullptr == World)
-		return;
-
+	if (nullptr == World) return;
 	TArray<FOverlapResult> OverlapResults;
 	FCollisionQueryParams CollisionQueryParam(NAME_None, false, ControllingPawn);
 	bool bResult = World->OverlapMultiByChannel(
@@ -43,24 +42,21 @@ void UBTService_Detect::TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeM
 
 	if (bResult)
 	{
-		for (auto OverlapResult : OverlapResults)
+		for (auto const& OverlapResult : OverlapResults)
 		{
 			AABCharacter* ABCharacter = Cast<AABCharacter>(OverlapResult.GetActor());
-
 			if (ABCharacter && ABCharacter->GetController()->IsPlayerController())
 			{
 				OwnerComp.GetBlackboardComponent()->SetValueAsObject(AABAIController::TargetKey, ABCharacter);
 				DrawDebugSphere(World, Center, DetectRadius, 16, FColor::Green, false, 0.2f);
 
-				DrawDebugPoint(World, ABCharacter->GetActorLocation(), 10.0f,
-					FColor::Blue, false, 0.2f);
-				DrawDebugLine(World, ABCharacter->GetActorLocation(), ABCharacter->GetActorLocation(),
-					FColor::Blue, false, 0.2f);
+				DrawDebugPoint(World, ABCharacter->GetActorLocation(), 10.0f, FColor::Blue, false, 0.2f);
+				DrawDebugLine(World, ControllingPawn->GetActorLocation(), ABCharacter->GetActorLocation(), FColor::Blue, false, 0.27f);
 				return;
 			}
 		}
 	}
-
-	OwnerComp.GetBlackboardComponent()->SetValueAsObject(AABAIController::TargetKey, nullptr);
+	else
+		OwnerComp.GetBlackboardComponent()->SetValueAsObject(AABAIController::TargetKey, nullptr);
 	DrawDebugSphere(World, Center, DetectRadius, 16, FColor::Red, false, 0.2f);
 }
