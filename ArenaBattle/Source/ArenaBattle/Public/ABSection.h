8b380a0229bf3abf49c0d1439e1dc00e29cbcaeb
Jungs <jungs6734@gmail.com>
2020-03-02 19:09:43 +0900

    Chapter 13 - 무한 맵의 생성 (문제:바닥 통과 현상)

---
+++ ArenaBattle/Source/ArenaBattle/Public/ABSection.h
@@ -0,0 +1,60 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "ArenaBattle.h"
+#include "GameFramework/Actor.h"
+#include "ABSection.generated.h"
+
+UCLASS()
+class ARENABATTLE_API AABSection : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AABSection();
+
+	virtual void OnConstruction(const FTransform& Transform) override;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+private:
+	enum class ESectionState : uint8
+	{
+		READY = 0,		// 액터의 시작 스테이트
+		BATTLE,			// 문을 닫고 일정 시간 뒤 NPC 소환
+		COMPLETE		// 닫힌 문을 연다. 배치한 트리거 게이트로 플레이어 감지
+	};
+
+	void SetState(ESectionState NewState);
+	ESectionState CurrentState = ESectionState::READY;
+
+	void OperateGates(bool bOpen = true);
+
+	UFUNCTION()
+	void OnTriggerBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, 
+		UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult &SweepResult);
+
+	UFUNCTION()
+	void OnGateTriggerBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, 
+		int32 OtherBodyIndex, bool bFromSweep, const FHitResult &SweepResult);
+
+private:
+	UPROPERTY(VisibleAnywhere, Category = Mesh, Meta = (AllowPrivateAccess = true))
+	TArray<UStaticMeshComponent*> GateMeshes;
+
+	UPROPERTY(VisibleAnywhere, Category = Trigger, Meta = (AllowPrivateAccess = true))
+	TArray<UBoxComponent*> GateTriggers;
+
+	UPROPERTY(VisibleAnywhere, Category = Mesh, Meta = (AllowPrivateAccess = true))
+	UStaticMeshComponent* Mesh;
+
+	UPROPERTY(VisibleAnywhere, Category = Trigger, Meta = (AllowPrivateAccess = true))
+	UBoxComponent* Trigger;
+
+	UPROPERTY(EditAnywhere, Category = State, Meta = (AllowPrivateAccess = true))
+	bool bNoBattle;
+};
